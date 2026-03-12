7b827b1b2705dd5489195ae3192bf014b5ebdf10
riodwimulyono0512-sudo <riodwimulyono0512@gmail.com>
2026-03-12 19:49:23 +0700

    jumlah

--- prak4.cpp
+++ prak4.cpp
@@ -5,3 +5,14 @@ int arr[20];              //membuat array dengan panjang data 20
 int n;                    //membuat variable inputan n
 
 void input()//prosedur input
+{
+    while (true){
+        cout << "Masukkan Jumlah Data Pada Array : ";
+        cin >> n;
+        if (n <= 20){
+            break;
+        }
+        else{
+            cout << "\nArray Tidak Boleh Lebih dari 20\n";
+        }
+    }
