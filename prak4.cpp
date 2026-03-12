98fb924926003187baeaff6daf7a7e9d0b871ded
riodwimulyono0512-sudo <riodwimulyono0512@gmail.com>
2026-03-12 19:53:17 +0700

    8

--- prak4.cpp
+++ prak4.cpp
@@ -55,3 +55,8 @@ void display(){
     cout << "\n=================";
     cout << "\nElemen Array" << endl;
     cout << "\n=================" << endl;
+
+    for(int j = 0; j < n; j++){
+        cout << arr[j] << endl;
+    }
+    cout << endl;
