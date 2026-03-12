e43f650ce14edc21d864dc40c01c075307a6e2ea
riodwimulyono0512-sudo <riodwimulyono0512@gmail.com>
2026-03-12 19:50:44 +0700

    elemen array

--- prak4.cpp
+++ prak4.cpp
@@ -9,10 +9,20 @@ void input()//prosedur input
     while (true){
         cout << "Masukkan Jumlah Data Pada Array : ";
         cin >> n;
+
         if (n <= 20){
             break;
         }
         else{
             cout << "\nArray Tidak Boleh Lebih dari 20\n";
         }
+    }
+    cout << endl;
+    cout << "=============================================";
+    cout << "==========Masukan Elemen Array==============";
+    cout << "=============================================" << endl;
+    
+    for (int i = 0; i < n; i++){
+        cout << "Data ke-" << (i + 1) << ": ";
+        cin >> arr[i];
     }
